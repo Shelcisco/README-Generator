fbd91f6a8e3a407f24ed6c1ff67456a34ffc122c
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-03-12 16:53:42 -0700

    correct typos in code

--- generator.js
+++ generator.js
@@ -1,96 +1,96 @@
 const inquirer = require('inquirer')
 const fs = require('fs');
-const { message }  = require ('statuses');
+// const { message }  = require ('statuses');
 
 //inquirer to generate questions
-inquirer.propmt(
+inquirer.prompt(
     [
         {
             
-            type: 'input',
-            message:"What's the project title?",
-            name:'Title',
-            //validate property to check the user provided input
-            validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},
-            },
-            {
-                type: 'input',
-                message:"Please describe your project",
-                name:'Description',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          type: 'input',
+          message:"What's the project title?",
+          name:'Title',
+          //validate property to check the user provided input
+          validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},
+          },
+          {
+              type: 'input',
+              message:"Please describe your project",
+              name:'Description',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-            {
-                type: 'input',
-                message:"Please provide installation instructions",
-                name:'Installation',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          {
+              type: 'input',
+              message:"Please provide installation instructions",
+              name:'Installation',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-            {
-                type: 'input',
-                message:"How is your project used?",
-                name:'Usage',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          {
+              type: 'input',
+              message:"How is your project used?",
+              name:'Usage',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-            {
-                type: 'input',
-                message:"Please list contributors",
-                name:'Contribution',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          {
+              type: 'input',
+              message:"Please list contributors",
+              name:'Contribution',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-            {
-                type: 'input',
-                message:"What's the project title?",
-                name:'Test',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          {
+              type: 'input',
+              message:"What's the project title?",
+              name:'Test',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-            {
-                type: 'list',
-                message:"Please choose a license",
-                name:'License',
-                choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please select option to continue'}},   
-            },
+          {
+              type: 'list',
+              message:"Please choose a license",
+              name:'License',
+              choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please select option to continue'}},   
+          },
 
-            {
-                type: 'input',
-                message:"What's your Github username?",
-                name:'Github',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          {
+              type: 'input',
+              message:"What's your Github username?",
+              name:'Github',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-            {
-                type: 'input',
-                message:"What's your email address?",
-                name:'Email',
-                //validate property to check the user provided input
-                validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
-            },
+          {
+              type: 'input',
+              message:"What's your email address?",
+              name:'Email',
+              //validate property to check the user provided input
+              validate: (value)=>{ if(value){return true} else {return 'Please input answer to continue'}},    
+          },
 
-          
         
-    ]
+      
+  ]
 ).then (({
-    Title,
-    Description,
-    Installation,
-    Usage,
-    Contribution,
-    Test,
-    License,
-    Github,
-    Email
+  Title,
+  Description,
+  Installation,
+  Usage,
+  Contribution,
+  Test,
+  License,
+  Github,
+  Email
 
 })=>
 {
@@ -102,20 +102,20 @@ inquirer.propmt(
     * [Contribution](#Contribution)
     * [Test](#Test)
     * [License](#License)
-    #Description
+    ##Description
     ${Description}
-    #Installation
+    ##Installation
     ${Installation}
-    #Usage
+    ##Usage
     ${Usage}
-    #Contribution
+    ##Contribution
     ${Contribution}
-    #Test
+    ##Test
     ${Test}
-    #License
+    ##License
     ${License}
 
-    # Questions
+    ## Questions
     * Github ;${Github}
     * Email ;${Email}`;
     
@@ -123,9 +123,9 @@ inquirer.propmt(
 }
 );
 
-function createNewFile(fileName,data){
+function createNewFile(generator,data){
 
-    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`,data,(err)=>{
+    fs.writeFile(`${generator.toLowerCase().split(' ').join('')}.md`,data,(err)=>{
 if(err){
     console.log(err)
 }
